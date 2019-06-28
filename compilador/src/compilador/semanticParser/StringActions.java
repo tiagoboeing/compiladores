519b4c3bfcbd59dc069b3f414f02df2419248654
gustavo henrique spiess <gustavo.spiess@philips.com>
2019-06-28 20:58:16 -0300

    Implementada constante inteira (\s \t e \n)

--- compilador/src/compilador/semanticParser/StringActions.java
+++ compilador/src/compilador/semanticParser/StringActions.java
@@ -4,13 +4,23 @@ import compilador.controller.SemanticError;
 import compilador.controller.Token;
 
 public enum StringActions implements SemanticAction {
-    CONST(21) {
+    CTE_STR(21) {
         @Override
         public void execute(SemanticParser parser, Token token) throws SemanticError {
             parser.pushType(SemanticTypes.string);
             parser.addCode("ldstr " + token.getLexeme());
         }
-    };
+    },
+    CTE_CHAR(20) {
+        @Override
+        public void execute(SemanticParser parser, Token token) throws SemanticError {
+            String lexeme = token.getLexeme();
+            if (lexeme.endsWith("s"))
+                lexeme = " ";
+            parser.pushType(SemanticTypes.string);
+            parser.addCode("ldstr \"" + lexeme + "\"");
+        }
+    },;
 
     private int id;
 
